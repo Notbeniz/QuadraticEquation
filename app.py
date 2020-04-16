4210335e2eda12f87e4db686ea6e671329cc95ee
Zeynab.S.Alavi <dvlpr1992@gmail.com>
2020-04-16 16:53:11 +0430

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import math
+x = input()
+a,b,c = x.split(' ')
+
+delta = math.sqrt((int(b)**2)-(4*int(a)*int(c)))
+
+r1 = (-int(b)+delta)/(2*int(a))
+r2 = (-int(b)-delta)/(2*int(a))
+
+print('{} {}'.format(r1,r2))
