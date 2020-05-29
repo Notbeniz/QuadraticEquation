b24fcfde9b161b972c69b89567f648ba78ea4b29
Pouria-Taghipour <59606980+Pouria83@users.noreply.github.com>
2020-05-29 09:56:33 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-import math
-x = input()
-a,b,c = x.split(' ')
+#!/usr/bin/python3
+form math import*
+a=int(input("Enter Your  Number A -->  :"))
+b=int(input("Enter Your  Number B -->  :"))
+c=int(input("Enter Your  Number C -->  :"))
+#********************************************
+print("********************************************")
+print("             ∆=0 Moadele Yek javab darad")
+print("             ∆>0 Moadele 2  javab darad")
+print("             ∆<0 Moadele javab nadarad")
+print("                   Formuls∆=(b^2-4ac)")
+print("                  Formuls X=(-b±√∆)/2a")
+print("********************************************")
+delta=b*b-4*a*c
+print("Delta =",delta)
 
-delta = math.sqrt((int(b)**2)-(4*int(a)*int(c)))
+#********************************************
 
-r1 = (-int(b)+delta)/(2*int(a))
-r2 = (-int(b)-delta)/(2*int(a))
+if delta==0:
+    x=(-b+sqrt(delta))/(2*a)
+    print("Moadele Yek javab darad.")
+    print("X=",x)
 
-print('{} {}'.format(r1,r2))
+#*******************************************   
+
+elif delta<0:
+        print("Moadele javab nadarad!")
+
+#*******************************************        
+else:
+
+        print("Moadele 2  javab darad")
+        x1=(-b+sqrt(delta))/(2*a)
+        x2=(-b+-math.sqrt(delta))/(2*a) 
+        print("X1=",x1)
+        print("X2=",x2)
+
+#*******************************************       
+
+#End
+        
