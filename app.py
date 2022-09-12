0deee46dd201d888a2a2f2add3d4e77a34ec4352
Mahdi salimkhani <55203977+mah5001@users.noreply.github.com>
2022-09-12 19:28:32 +0430

    Update app.py
    Edited Delta & Aded conditions

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ x = input()
 
 a,b,c = x.split(' ')
 
-delta = math.sqrt((int(b)**2)-(4*int(a)*int(c)))
-
-r1 = (-int(b)+delta)/(2*int(a))
-r2 = (-int(b)-delta)/(2*int(a))
-
-print(f'{r1} {r2}')
+delta = ((int(b)**2)-(4*int(a)*int(c)))
+if delta >0:
+  r1 = (-int(b)+math.sqrt(delta))/(2*int(a))
+  r2 = (-int(b)-math.sqrt(delta))/(2*int(a))
+  print(f'{r1} {r2}')
+if delta==0:
+  r = (-int(b))/(2*int(a))
+  print(f'{r}')
+if delta<0:
+   print("Equation Answers is NaN, Because delta is Negative")
